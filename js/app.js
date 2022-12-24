c8fb5dbbcfaa8c4733010d8e7552866c729b8a25
fedemelgar <fedemelgar09@gmail.com>
2022-12-23 20:23:49 -0600

    Add hover feature to button +

--- js/app.js
+++ js/app.js
@@ -3,11 +3,15 @@
 
 var main = function () {
 "use strict";
+
 $(".comment-input button").on("click", function (event) {
 var $new_comment = $("<p>"),
 comment_text = $(".comment-input input").val();
 $new_comment.text(comment_text);
 $(".comments").append($new_comment);
+$(".comment-input input").val("");
 });
-};
+
+
+
 $(document).ready(main);
